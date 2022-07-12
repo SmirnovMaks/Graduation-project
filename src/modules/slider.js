ca8b0a662a3331ecdc34d43a70cf835f5ee15e6c
Maksim Smirnov <perforator01@gmail.com>
2022-07-13 01:22:55 +0300

    слайдер

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,17 +1,27 @@
 import Swiper, {
     Navigation,
-    Pagination
+    Pagination,
+    Autoplay
 } from 'swiper';
 
 const slider = () => {
     const swiper = new Swiper('.services-elements', {
-        modules: [Navigation, Pagination],
+        modules: [Navigation, Pagination, Autoplay],
         navigation: {
             nextEl: '.arrow-right',
             prevEl: '.arrow-left'
         },
         slidesPerView: 3,
         loop: true
+    });
+    const topSlider = new Swiper('.top-slider', {
+        modules: [Navigation, Autoplay],
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        }
     });
 };
 
